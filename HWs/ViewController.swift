113a8277abb41b2ed4f88d330a336a69e2ad864f
Maxim Kaplia <kapliamaks@gmail.com>
2023-04-02 19:47:18 +0300

    just adds 3 squares in random places at this point

--- HWs/ViewController.swift
+++ HWs/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(gestureFunc))
+        
+        view.addGestureRecognizer(gesture)
+  
     }
+    
+    
+    
+    @objc func gestureFunc(){
 
+        for _ in 0 ..< 3 {
+        print ("tap")
+        let randomX = CGFloat.random(in: 0...200)
+        let randomY = CGFloat.random(in: 0...200)
+        let randomH = CGFloat.random(in: 20...60)
+        let randomW = CGFloat.random(in: 20...60)
+        let anotherView = UIView()
+        anotherView.frame = CGRect (x: randomX, y: randomY, width: randomW, height: randomH)
+        anotherView.backgroundColor = .purple
+        
+        view.addSubview(anotherView)
+        
+        }
+        
+        
+        
+    }
+    
+    
+   
+    
 
 }
 
