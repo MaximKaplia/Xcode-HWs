20b97fff7e0f7b3a2919c7f07cd04a1dd5dab5f1
Maxim Kaplia <kapliamaks@gmail.com>
2023-04-11 22:39:34 +0300

    added SnapKit, Basic Setup

--- HWs/ViewController.swift
+++ HWs/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupButton(startButton)
     }
 
 
+    @IBAction func gameStart(_ sender: UIButton) {
+        
+        let  storyborad = UIStoryboard(name: "Main", bundle: nil)
+        guard let destinationVC = storyborad
+                .instantiateViewController(identifier: "GameController") as? GameController,
+              let navController = navigationController
+              else {return}
+        navController.pushViewController(destinationVC, animated: true)
+        
+        
+        
+    }
+    
+    private func setupButton(_ button:UIButton) {
+        
+        button.layer.cornerRadius = button.frame.height / 2
+        
+    }
+    
+    private func setupSprite (_ sprite: UIView) {
+        
+        
+        
+    }
 }
 
